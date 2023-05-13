845881a1835d22c2b5483ff3afb6dd49eef4c7ae
gitscoobs <46730004+gitscoobs@users.noreply.github.com>
2023-05-13 13:21:01 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,1 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  menu.classList.toggle('menu-open');
-});
